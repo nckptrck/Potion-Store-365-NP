f2c28c33d572de45b61184f5a70fec791faf5827
nckptrck <nckptrck1@gmal.com>
2023-10-09 12:23:01 -0700

    still trying to fix barrels

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -29,11 +29,15 @@ def post_deliver_barrels(barrels_delivered: list[Barrel]):
          total_price += barrel.price * barrel.quantity
          ml_in_barrels = barrel.ml_per_barrel * barrel.quantity
          
-         with db.engine.begin() as connection:
-            connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_ml = num_red_ml + %s", (ml_in_barrels)))
-    
+         update_ml_query = sqlalchemy.text("UPDATE global_inventory SET num_red_ml = num_red_ml + :ml_in_barrels")
+        
+    update_gold_query = sqlalchemy.text("UPDATE global_inventory SET gold = gold - :total_price")
+
+
     with db.engine.begin() as connection:
-        connection.execute(sqlalchemy.text("UPDATE global_inventory SET gold = gold - %s", (total_price)))
+        connection.execute(update_ml_query, ml_in_barrels=ml_in_barrels)
+        connection.execute(update_gold_query, total_price=total_price)
+
   
 
 
@@ -45,7 +49,7 @@ def post_deliver_barrels(barrels_delivered: list[Barrel]):
 @router.post("/plan")
 def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
     """ """
-    print(wholesale_catalog)
+    print("Catalog: " wholesale_catalog)
 
     with db.engine.begin() as connection:
         potions_result = connection.execute(sqlalchemy.text("SELECT num_red_potions, gold FROM global_inventory"))

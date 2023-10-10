66bd9d9b7c7511321591cf3df1cd3f044b767705
nckptrck <nckptrck1@gmal.com>
2023-10-09 20:04:49 -0700

    changes to catelog

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -14,27 +14,35 @@ def get_catalog():
 
     # Can return a max of 20 items.
     with db.engine.begin() as connection:
-        potions_result = connection.execute(sqlalchemy.text("SELECT num_red_potions FROM global_inventory"))
+        potions_result = connection.execute(sqlalchemy.text("SELECT num_red_potions, num_green_potions, num_blue_potions FROM global_inventory"))
         row = potions_result.first()
         
     red_potions = row[0]
-    if red_potions > 0:
-        return [
+    green_potions = row[1]
+    blue_potions = row[2]
+
+    return [
                 {
                     "sku": "RED_POTION_0",
                     "name": "red potion",
-                    "quantity": 1,
+                    "quantity": red_potions,
                     "price": 50,
                     "potion_type": [100, 0, 0, 0],
-                }
-        ]
-    else:
-        return [
+                },
                 {
-                    "sku": "RED_POTION_0",
-                    "name": "red potion",
-                    "quantity": 0,
+                    "sku": "GREEN_POTION_0",
+                    "name": "green potion",
+                    "quantity": green_potions,
                     "price": 50,
-                    "potion_type": [100, 0, 0, 0],
+                    "potion_type": [0, 100, 0, 0],
+                },
+                {
+                    "sku": "BLUE_POTION_0",
+                    "name": "blue potion",
+                    "quantity": blue_potions,
+                    "price": 50,
+                    "potion_type": [0, 0, 100, 0],
                 }
+
         ]
+    

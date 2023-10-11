e0d11e6f4ff0e2f2d787808fce50999c4d5162ef
nckptrck <nckptrck1@gmal.com>
2023-10-10 20:11:08 -0700

    cc

--- src/api/admin.py
+++ src/api/admin.py
@@ -23,7 +23,7 @@ def get_shop_info():
 
     # TODO: Change me!
     return {
-        "shop_name": "Potion Shop",
-        "shop_owner": "Potion Seller",
+        "shop_name": "Patrick's Powerful Potions",
+        "shop_owner": "Nick Patrick",
     }
 

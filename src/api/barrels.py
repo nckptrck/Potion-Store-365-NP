11f5a788c36b84da006fe3b523d1e73b5025d299
nckptrck <nckptrck1@gmal.com>
2023-10-13 20:29:57 -0700

    barrels

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -31,15 +31,15 @@ def post_deliver_barrels(barrels_delivered: list[Barrel]):
         
         if pot_type == [1,0,0,0]: #RED
             with db.engine.begin() as connection:
-                update_ml_query = sqlalchemy.text("UPDATE resources SET num_red_ml = num_red_ml + :ml_in_barrels")
+                update_ml_query = sqlalchemy.text("UPDATE resources SET red_ml = red_ml + :ml_in_barrels")
                 connection.execute(update_ml_query, parameters=(dict(ml_in_barrels=ml_in_barrels)))
         elif pot_type == [0,1,0,0]: #GREEN
             with db.engine.begin() as connection:
-                update_ml_query = sqlalchemy.text("UPDATE resources SET num_green_ml = num_green_ml + :ml_in_barrels")
+                update_ml_query = sqlalchemy.text("UPDATE resources SET green_ml = green_ml + :ml_in_barrels")
                 connection.execute(update_ml_query, parameters=(dict(ml_in_barrels=ml_in_barrels)))
         elif pot_type == [0,0,1,0]: #BLUE
             with db.engine.begin() as connection:
-                update_ml_query = sqlalchemy.text("UPDATE resources SET num_blue_ml = num_blue_ml + :ml_in_barrels")
+                update_ml_query = sqlalchemy.text("UPDATE resources SET blue_ml = blue_ml + :ml_in_barrels")
                 connection.execute(update_ml_query, parameters=(dict(ml_in_barrels=ml_in_barrels)))
 
 

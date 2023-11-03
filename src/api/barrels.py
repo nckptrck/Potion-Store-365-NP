cdfb7a5a432fda9788e7acfdb1e18377ef0ea732
nckptrck <nckptrck1@gmal.com>
2023-11-02 22:30:38 -0700

    barrels

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -181,6 +181,13 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
                                "quantity": 1})
         barrels_copped.append({"sku": "SMALL_RED_BARREL",
                                "quantity": 1})
+    elif gold >= 960:
+        barrels_copped.append({"sku": "SMALL_BLUE_BARREL",
+                               "quantity": 3})
+        barrels_copped.append({"sku": "SMALL_RED_BARREL",
+                               "quantity": 3})
+        barrels_copped.append({"sku": "SMALL_GREEN_BARREL",
+                               "quantity": 3})
     elif gold >= 320:
         barrels_copped.append({"sku": "SMALL_BLUE_BARREL",
                                "quantity": 1})
